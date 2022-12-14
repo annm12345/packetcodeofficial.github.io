c91c7543d39f5878a1296cc027e452c29610c204
annm12345 <aungnyinyimin32439@gmail.com>
2022-12-14 10:22:26 +0630

    Added new files

---
+++ main.js
@@ -0,0 +1,55 @@
+const bar= document.getElementById('bar');
+const close= document.getElementById('close');
+const nav= document.getElementById('navbar');
+
+
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
+}
+
+let searchForm=document.querySelector(".search-bar");
+
+document.querySelector(".search-btn").onclick=()=>{
+    searchForm.classList.toggle('active');
+}
+
+var swiper = new Swiper(".slider", {
+    autoloop:true,
+    spaceBetween: 30,
+    hashNavigation: {
+      watchState: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new swiper(".pslide", {
+    loop:true,
+    spaceBetween: 30,
+    hashNavigation: {
+      watchState: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
